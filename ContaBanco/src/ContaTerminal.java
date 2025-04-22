0ca129c35a272000e2929baa0f2e3283b2c1c4e3
JAndersonArruda <ferrazarrudaanderson@gmail.com>
2025-04-22 20:10:22 -0300

    feat: add basic bank logic

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+
+        Integer numero;
+        String agencia;
+        String nome;
+        double saldo;
+
+        System.out.println("Por favor, informe seu nome: ");
+        nome = scan.nextLine();
+
+        System.out.println("Por favor, informe o número da sua agencia: ");
+        agencia = scan.nextLine();
+
+        System.out.println("Por favor, informe o número da sua conta: ");
+        numero = scan.nextInt();
+        scan.nextLine();
+
+        System.out.println("Por favor, informe o valor inicial da sua conta: ");
+        saldo = scan.nextDouble();
+        scan.nextLine();
+
+        System.out.println("\nOlá " + nome +
+                ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
+                ", conta " + numero +
+                " e seu saldo " + String.format("%.2f", saldo) +
+                " já está disponível para saque.");
     }
 }
